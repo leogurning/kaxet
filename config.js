e10d7d12e5f8455a3aba800beebec2c0454de1b2
leo gurning <leogurning@gmail.com>
2017-12-02 02:31:28 +0700

    add node start

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'serverport':8080,
+    'serverport':2000,
     'tokenexp': 3600,
     'secret': '!$3cret4@uth',
     'database': 'mongodb://admindb:kaxetadmin@ds127506.mlab.com:27506/kaxetdb',
