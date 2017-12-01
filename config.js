d0069c696b8542d563761bcbb518f52546ecc7db
leo gurning <leogurning@gmail.com>
2017-12-02 02:09:34 +0700

    add remote db mlab

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     'serverport':8080,
     'tokenexp': 3600,
     'secret': '!$3cret4@uth',
-    'database': 'mongodb://localhost:27017/kaxetdb',
+    'database': 'mongodb://admindb:kaxetadmin@ds127506.mlab.com:27506/kaxetdb',
     'baseuploadpath': ''
  };
